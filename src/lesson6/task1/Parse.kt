0ab5108a8b6a262e0e43bdcb8853c80abbe4e928
Qustarnick <qustarnick@gmail.com>
2021-12-13 20:22:48 +0300

    fun findSumOfTwo() fix again

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -177,34 +177,6 @@ fun mostExpensive(description: String): String = TODO()
  */
 fun fromRoman(roman: String): Int = TODO()
 
-//var rz:string;
-//    n,ar,ar1,i,a: integer;
-//
-//Begin
-//clrscr;
-//Writeln ('Liczba rzymska');
-//Readln(rz);
-//ar:=0; n:=0;
-//for i := 1 to length (rz) do
-//  begin
-//  ar1:=ar;
-//    if rz[i]='I' then ar:=1;
-//    if rz[i]='V' then ar:=5;
-//    if rz[i]='X' then ar:=10;
-//    if rz[i]='L' then ar:=50;
-//    if rz[i]='C' then ar:=100;
-//    if rz[i]='D' then ar:=500;
-//    if rz[i]='M' then ar:=1000;
-//
-//    if ar>ar1 then a:=-2*ar1
-//              else a:=0;
-//    n:=n+a+ar
-//  end;
-//
-// Writeln ('Arabska =  ',n);
-// Readln;
-//end.
-
 /**
  * Очень сложная (7 баллов)
  *
